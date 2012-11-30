b57f4d1709167ec28d4787c2ea1629064efc838b
The Jaaj <jaja.s@kiranatama.com>
2012-11-30 17:00:16 +0700

    test 123 percobaan test 123

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hellotest::Application.config.secret_token = 'cce48d7ff1cef9a34417e50b237ddad67c5ee317cf8ee212753e3ca2575404755ef947c10c2b6fd65015bbf3c6d6af98430250829959920d86c3f45d5d5aed42'
